a0703beae693c7c677d12438fabc9e6af82ffbed
Gilmar Basilio <gilmar.basilio@gmail.com>
2019-09-29 09:32:52 -0300

    incluing providers

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
-import FileController from './app/controllers/FileController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -16,6 +18,8 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
