f3db04427a49f6905a52dbf5e3d3b8350a416f07
gilmarbasilio <gilmar.basilio@gmail.com>
2019-09-29 10:12:36 -0300

    Incluindo agendamento

---
+++ src/app/models/Appointment.js
@@ -0,0 +1,35 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class Appointment extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        date: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
+}
+
+export default Appointment;
