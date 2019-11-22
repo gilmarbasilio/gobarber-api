5a3ed6ff398aa3098a7d827808aec522e444351a
GIlmar Basilio <gilmar.basilio@gmail.com>
2019-11-22 11:58:46 -0300

    ajustes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import 'dotenv/config';
+
 import express from 'express';
-import 'express-async-errors';
 import path from 'path';
-import * as Sentry from '@sentry/node';
-import sentryConfig from './config/sentry';
-import routes from './routes';
-import './database';
+import cors from 'cors';
 import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
+
+import routes from './routes';
+import sentryConfig from './config/sentry';
+
+import './database';
 
 class App {
   constructor() {
@@ -21,6 +25,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
